0ec9c6db709d4625b04fbd3aee810d7f4a95adfd
JuJinPark <wnwls1216@gmail.com>
2019-06-09 21:14:55 +0900

    1단계완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +28,65 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+       
+        
+        	String url=findRequestedURL(in);
+            
+            
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            
+            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+       
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private String findRequestedURL(InputStream in) throws IOException {
+     	
+     	String url;
+     	BufferedReader rd= new BufferedReader(new InputStreamReader(in,"UTF-8"));
+    	
+     	String data=rd.readLine();
+     	if(data==null) {
+     		return null;
+     	}
+     	url=data.split(" ")[1];
+     	
+     	
+     	
+    	while(!"".equals(data)) {
+   	log.debug("headers : {}",data);
+  		data=rd.readLine();
+    	
+    	}
+    	return url;
+    	
+    	
+    }
+    
+    private void checkinput2(InputStream in) throws IOException {
+     	
+     	
+     	BufferedReader rd= new BufferedReader(new InputStreamReader(in,"UTF-8"));
+     	StringBuilder response= new StringBuilder();
+     	int data=rd.read();
+     	if(data==-1) {
+     		return;
+     	}
+     	
+     	
+    	while(data!=-1) {
+//   	log.debug("headers : {}",data);
+  		data=rd.read();
+  		response.append( (char)data) ;  
+    	
+    	}
+    	System.out.print(response);
+    	
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
