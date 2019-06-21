83c2f998ffdb62e4305e9a6669e2e03b94f55ba6
JuJinPark <wnwls1216@gmail.com>
2019-06-22 00:44:31 +0900

    요구사항5

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,20 +7,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    
+    public static DataBase users;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -33,24 +39,51 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
         	
-       
-        
-        	String url=findRequestedURL(in);
-            
+        	BufferedReader rd= new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        	Map<String,String> Http= new HashMap();
+         	readHeader(rd,Http);
+//         	if(header==null) {
+//         		return;
+//         	}
+//        
+//         	String requestType=header[0];
+//         	String url=header[1];
+//            
         	DataOutputStream dos = new DataOutputStream(out);
             byte[] body = null;
-            if(url.indexOf("html")!=-1) {
-            	body=Files.readAllBytes(new File("./webapp"+url).toPath());
-          
-            }else if(url.startsWith("/user/create")) {
-            	int indx=url.indexOf("?");
-            	String queryString=url.substring(indx+1);
-            	usercreatehandler(queryString);
+            
+            if("POST".equals(Http.get("requestType"))) {
+            	
+            	if("/user/login".equals(Http.get("url"))) {
+            		
+            		
+            		response200HeaderForLogin(dos,login(Http.get("userId"),Http.get("password")));
+            		
+            		
+            		if(login(Http.get("userId"),Http.get("password"))) {
+            			//일단 200응답하고 쿠킵갑으로 어디로 갈지 판단해야함
+            			response302Header(dos,"/index.html");
+            		}else {
+            			response302Header(dos,"/user/login_failed.html");
+            		}
+            		
+            	}else if("/user/create".equals(Http.get("url"))) {
+            		String content=IOUtils.readData(rd,Integer.parseInt(Http.get("Content-Length")));
+                	
+            		usercreatehandler(content);
+                	response302Header(dos,"/index.html");
+            	}
             	
             	
-            	body="저장성공".getBytes();
+            	
+            	
+            	
+            }else {
+            	body=Files.readAllBytes(new File("./webapp"+Http.get("url")).toPath());
+            	
+            	response200Header(dos, body.length);
             }
-            response200Header(dos, body.length);
+            
             responseBody(dos, body);
             
          
@@ -59,64 +92,106 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
+    
+    private boolean login(String id,String pwd) {
+    	User member=users.findUserById(id);
+//    	if(users.findUserById(id)==null) {
+//    		return false;
+//    	}
+    	if(member != null && member.getPassword().equals(pwd)) {
+    		return true;
+    	}
+    	return false;
+    	
+    }
     private void usercreatehandler(String url) {
     
     	Map<String,String> userinfo=HttpRequestUtils.parseQueryString(url);
     	User user=new User(userinfo.get("userId"),userinfo.get("password"),userinfo.get("name"),userinfo.get("email"));
+    	users.addUser(user);
+    	
     	log.debug("userinfo - id :{}, passward:{}, name:{}, email:{}",user.getUserId(),user.getPassword(),user.getName(),user.getEmail());
+    }
+//    private String readBody(BufferedReader rd,int Contentlength) throws IOException {
+//    	
+//    }
+    
+    private void readHeader(BufferedReader rd,Map<String,String> map) throws IOException {
+    	
+    	
+	
+     	
+    	
+     	String data=rd.readLine();
+     	if(data==null) {
+     		return;
+     	}
+     	log.debug("headers : {}",data);
+     	
+     	
+     	String[] header = data.split(" ");
+     	map.put("requestType",header[0]);
+     	map.put("url",header[1]);
+     	map.put("httpversion",header[2]);
+     	
+     	do {
+     		data=rd.readLine();
+         	log.debug("headers : {}",data);
+         	if("".equals(data)) return;
+         	String[] info=data.split(":");
+	  		map.put(info[0].trim(),info[1].trim());
+     	} while(!"".equals(data));
+//     	{
+//    	  
+//    	   	
+//    	  		
+//    	  		
+//    	  		
+//    	  	 	
+//    	  	
+//    	  	 	data=rd.readLine();
+//    	  	 	log.debug("headers : {}",data);
+//    	    	
+//    	   }
+     	
+     	
+     	
     }
     
  
     
-    private String findRequestedURL(InputStream in) throws IOException {
-     	
-     	String url;
-     	BufferedReader rd= new BufferedReader(new InputStreamReader(in,"UTF-8"));
-    	
-     	String data=rd.readLine();
-     	if(data==null) {
-     		return null;
-     	}
-     	url=data.split(" ")[1];
-     	
-     	
-     	
-    	while(!"".equals(data)) {
-   	log.debug("headers : {}",data);
-  		data=rd.readLine();
-    	
-    	}
-    	return url;
-    	
-    	
+   
+
+    private void response200HeaderForLogin(DataOutputStream dos,Boolean login) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Set-Cookie: logined=" + login.toString() + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
     
-    private void checkinput2(InputStream in) throws IOException {
-     	
-     	
-     	BufferedReader rd= new BufferedReader(new InputStreamReader(in,"UTF-8"));
-     	StringBuilder response= new StringBuilder();
-     	int data=rd.read();
-     	if(data==-1) {
-     		return;
-     	}
-     	
-     	
-    	while(data!=-1) {
-//   	log.debug("headers : {}",data);
-  		data=rd.read();
-  		response.append( (char)data) ;  
-    	
-    	}
-    	System.out.print(response);
-    	
-    }
+    
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    
+
+    private void response302Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: "+url+"\r\n");
+
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
