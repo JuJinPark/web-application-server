f74961dd3647b6e33bad4cbe597b5d47b7541ca9
JuJinPark <wnwls1216@gmail.com>
2019-07-10 23:58:29 +0900

    리팩토링중

--- src/main/java/webserver/UserCreateHandler.java
+++ src/main/java/webserver/UserCreateHandler.java
@@ -1,14 +1,88 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
+
 public class UserCreateHandler implements RequestManagable {
-	String url;
-	UserCreateHandler(){
+
+	Map<String, String> headerInfo;	
+	BufferedReader inputstream;
+	
+//	
+//	String content=IOUtils.readData(rd,Integer.parseInt(headerInfo.get("Content-Length")));
+//	//정보 저장
+//	saveUser(content);
+//	//응답헤더 작성
+//	responseHeader.append(IOUtils.StatusHeaderMaker("302","Redirect"))
+//	.append(IOUtils.otherResposeHeaderMaker("Location","/index.html"))
+// 	.append("\r\n");
+//	 dos.writeBytes(responseHeader.toString());
+	
+//    private void saveUser(String url) {
+//        
+//    	Map<String,String> userinfo=HttpRequestUtils.parseQueryString(url);
+//    	User user=new User(userinfo.get("userId"),userinfo.get("password"),userinfo.get("name"),userinfo.get("email"));
+//    	DataBase.addUser(user);
+//    	
+//    	log.debug("userinfo - id :{}, passward:{}, name:{}, email:{}",user.getUserId(),user.getPassword(),user.getName(),user.getEmail());
+//    }
+
+	UserCreateHandler(Map<String,String> headerInfo,BufferedReader inputstream){
+		this.headerInfo=headerInfo;
+		this.inputstream=inputstream;
+	}
+	
+	@Override
+	public void response(DataOutputStream dos){
+		// TODO Auto-generated method stub
+		String url=getBody();
+		User member=createUser(HttpRequestUtils.parseQueryString(url));
+		DataBase.addUser(member);
+//		setUserInfo(getBody());
+//		
+//		if(checkPassword(getUser(userinfo.get("userId")),userinfo.get("password"))) {
+//			loginSucessResponse(dos);
+//			return;
+//		}
+//    
+//		
+//		loginFailedResponse(dos);
 		
 	}
-		@Override
-		public String getUrl() {
-			// TODO Auto-generated method stub
-			return url;
+	
+	public User createUser(Map<String,String> userinfo) {
+		return new User(userinfo.get("userId"),userinfo.get("password"),userinfo.get("name"),userinfo.get("email"));
+	}
+	
+	public void saveUser(User member) {
+		DataBase.addUser(member);
+	}
+	
+	
+	private String getBody() {
+		try {
+			return IOUtils.readData(inputstream,Integer.parseInt(headerInfo.get("Content-Length")));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return null;
+	}
+	
+//	LoginHandler(Map<String,String> headerInfo,BufferedReader inputstream){
+//		this.headerInfo=headerInfo;
+//	this.inputstream=inputstream;
+//	}
+
 
 }
--- src/test/java/webserver/CreateHandlerTest.java
+++ src/test/java/webserver/CreateHandlerTest.java
@@ -9,7 +9,9 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import db.DataBase;
 import model.User;
+import util.HttpRequestUtils;
 
 
 
@@ -28,13 +30,7 @@ public class CreateHandlerTest {
 	public void loginHandelrsetUser() {
 	 RequestHandlerFactory factory = new simpleRequestHandlerFactory();
 	 Map<String,String> headerInfo= new HashMap();
-//	 headerInfo.put("url","/user/login");
-////	 RequestManagable requestHandler=factory.getHandler(headerInfo, null);
-////	 
-//	 
-//	// requestHandler.createResponseHeader();
-//	 //requestHandler.createResponseBody();
-//	
+
 
 	 LoginHandler myLoginHandler = new LoginHandler(headerInfo,null);
 	 myLoginHandler.setUserInfo("userId=wnwls&password=1216");
@@ -52,4 +48,20 @@ public class CreateHandlerTest {
 		 User member = new User("wnwls","1216","jujin","ss@dd");
 		assertTrue(myLoginHandler.checkPassword(member,"1216"));
 	 }
+	 
+	 @Test
+	public void isSaved() {
+		 UserCreateHandler myUsderCreateHandler = new UserCreateHandler(null,null);
+		 User member = new User("wnwls","1216","jujin","ss@dd");
+		 String url="userId=wnwls&password=1216&name=jujin&email=ss@dd";
+		 Map<String,String> userinfo=HttpRequestUtils.parseQueryString(url);
+		 User member2=myUsderCreateHandler.createUser(userinfo);
+		 myUsderCreateHandler.saveUser(member);
+		// assertEquals(member2.toString(),member.toString());
+		assertEquals(member.toString(),DataBase.findUserById("wnwls").toString());
+		
+		
+	 }
+	
+		
 }
